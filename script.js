f352c2fab3e4db7a23db3b99ba2e6f4bf0618ab2
eduym17 <eduardo_yanez@live.com.mx>
2022-03-18 12:09:53 -0600

    Add functionality and DOM manipulation

---
+++ script.js
@@ -0,0 +1,101 @@
+let circleTurn;
+const xClass = 'x';
+const oClass = 'o';
+const board = document.getElementById('board');
+const resultMsgElement = document.getElementById('result-msg');
+const restartButton = document.getElementById('restartbtn');
+const winningMsgTxtElement = document.querySelector('[data-winning-message-text]');
+
+const winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
+const cellElements = document.querySelectorAll('[data-cell]');
+
+startGame();
+
+restartButton.addEventListener('click', startGame);
+
+function startGame() {
+  circleTurn = false;
+
+  cellElements.forEach(cell => {
+    cell.classList.remove(oClass);
+    cell.classList.remove(xClass);
+    cell.removeEventListener('click', handleClick);
+    cell.addEventListener('click', handleClick, { once: true});
+  });
+
+  showClassHover();
+  resultMsgElement.classList.remove('show');
+};
+
+function handleClick(event) {
+  const cell = event.target
+  const currentClass = circleTurn ? oClass : xClass;
+  placeMark(cell, currentClass);
+
+  if (checkWin(currentClass)) {
+    endGame(false)
+  } else if (isDraw()){
+    endGame(true)
+  } else {
+    switchTurn();
+    showClassHover();
+  }
+}
+
+function isDraw() {
+  return [...cellElements].every(cell => {
+    return cell.classList.contains(oClass) || cell.classList.contains(xClass)
+  })
+}
+
+function endGame(draw) {
+  if(draw) {
+    winningMsgTxtElement.innerText = 'Draw!';
+  }
+
+  if(!draw) {
+    winningMsgTxtElement.innerText = `${circleTurn ? "O's" : "X's"} wins!`
+  }
+  resultMsgElement.classList.add('show')
+}
+
+function placeMark(cell, currentClass) {
+  cell.classList.add(currentClass)
+}
+
+function switchTurn() {
+  circleTurn = !circleTurn;
+}
+
+function showClassHover() {
+  board.classList.remove(xClass);
+  board.classList.remove(oClass);
+
+  if (circleTurn) {
+    board.classList.add(oClass);
+  }
+
+  if (!circleTurn) {
+    board.classList.add(xClass);
+  }
+};
+
+function checkWin(currentClass) {
+  return winningCombinations.some(combination => {
+    return combination.every(index => {
+      return cellElements[index].classList.contains(currentClass);
+    })
+  })
+}
